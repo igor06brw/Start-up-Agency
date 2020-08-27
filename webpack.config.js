6cec7222eaa906f237ee0125217e7f13f9864136
Igor Borowski <igor06brw@gmail.com>
2020-08-27 09:11:33 +0200

    Add webpack ts, sass compiler & html watching

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,44 @@
 const path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/dist"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  },
-  plugins: [new MiniCssExtractPlugin()],
+  mode: 'development',
+  entry: "./src/index.ts",
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
     ]
-  }
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  stats: {
+    colors: true
+  },
+  devServer: {
+    contentBase: './dist',
+    writeToDisk: true,
+    watchContentBase: true,
+    port: 3456
+  },
+  output: {
+    path: path.resolve("dist"),
+    filename: "from-typescript.js",
+    publicPath: "/dist"
+  },
 };
